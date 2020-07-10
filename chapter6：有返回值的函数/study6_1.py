9e8522a63e954192efaa2819150a5807b4c1d8df
liangjian314 <hjybmlj@163.com>
2020-07-10 17:16:12 +0800

    新增计算阶乘代码

--- chapter6：有返回值的函数/study6_1.py
+++ chapter6：有返回值的函数/study6_1.py
@@ -37,6 +37,7 @@ print(compare(-2, -1))
 
 
 # 运用增量开发方式，写一个叫作 hypotenuse 的函数，接受直角三角形的两直角边长作为实参，返回该三角形斜边的长度。记录下你开发过程中的每一步
+
 def hypotenuse(a, b):
     z = a**2+b**2
     return(math.sqrt(z))
@@ -46,6 +47,7 @@ print(hypotenuse(3, 4))
 
 
 # 知道圆心和圆周上一点的横纵坐标，计算圆的面积
+
 def distance(x1, y1, x2, y2):
     a1 = x2-x1
     b1 = y2-y1
@@ -63,6 +65,7 @@ def circle_area(xc, yc, xp, yp):
 
 print(circle_area(1, 2, 4, 6))
 
+
 # 写一个函数 is_between(x, y, z) ，如果x<=y<=z 返回 True 否则返回 False。
 
 
@@ -74,3 +77,23 @@ def is_between(x, y, z):
 
 
 print(is_between(0, -2, 1))
+
+
+# 计算阶乘
+
+
+def factorial(n):
+    if not isinstance(n, int):
+        print('小数无阶乘。')
+        return None
+    elif n < 0:
+        print('负数无阶乘。')
+        return None
+    elif n == 0:
+        return 1
+    else:
+        recurse = factorial(n-1)
+        return n*factorial(n-1)
+
+
+print(factorial(5))
