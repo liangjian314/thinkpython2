5d9f81a81b9beac027d92469fb277d1e89e2610d
liangjian314 <hjybmlj@163.com>
2020-08-05 22:47:17 +0800

    提交练习题

---
+++ 其他/文件检索/play.py
@@ -0,0 +1,10 @@
+def find_keyword():
+    file = open('D:\学习\python3\其他\文件检索\log.log')
+    keys = ['question4', 'question1', 'exception']
+    for (num, keyword) in enumerate(file):
+        for word in keys:
+            if keyword.find(word) != -1:
+                print(num, keyword)
+
+
+find_keyword()
